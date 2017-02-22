11c405681444e201fa80f16db09f7a8f88ef4043
zhWang <wangzhenhaois@gmail.com>
2017-02-22 18:00:37 +0800

    update

--- httpget.py
+++ httpget.py
@@ -1,14 +1,17 @@
 import urllib2
 import urllib
 import time
+import re
 
 url = 'http://news.mydrivers.com/'
 user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'
 
 i=0
 while (i<5):
+    print i
     timestamp = int(round(time.time() * 1000))
-    values = {"pageid":'5','cids':'','timestamp':timestamp,'callback':'NewsList','_':timestamp}
+    print timestamp
+    values = {"pageid":i,'cids':'','timestamp':timestamp,'callback':'NewsList','_':timestamp}
 # headers= {'User-Agent':user_agent
 #          ,'Cookie':'JSESSIONID=50C8B6B6DB530B00ED9942A01C4AE27A; _gscu_1924971701=877505915xrsx786; _gscs_1924971701=87750591hlo3np86|pv:2; _gscbrs_1924971701=1'
 #          ,'Connection':'keep-alive'
@@ -23,8 +26,11 @@ while (i<5):
     #print responses.read()
     file_object = open(r'D:\python\test.txt','a')
     try:
-        file_object.write(responses.read())
+         str=re.findall('<div class="title"><span><a href=".*?htm">',responses.read())
+         print str
+         file_object.write(responses.read())
+         file_object.write('\n\n\n\n\n\n')
     finally:
-        file_object.close()
+         file_object.close()
     i = i+1
-    print i
+
